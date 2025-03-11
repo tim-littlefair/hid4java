2bbd3ff6a3bad5029da80f3e35142357e4aa4b4d
Tim Littlefair <5130151+tim-littlefair@users.noreply.github.com>
2025-03-11 17:37:10 +0800

    Synchronized to corresponding classes in FHAU

--- src/test/java/org/hid4java/examples/FMICStartupExample.java
+++ src/test/java/org/hid4java/examples/FMICStartupExample.java
@@ -274,10 +274,8 @@ abstract class FMICProtocolBase {
     }
   }
 
-  protected static void log(String ansiPrefix, String message) {
-    System.out.println(
-      ansiPrefix + message
-    );
+    protected static void log(String message) {
+        System.out.println(message);
   }
 }
 
@@ -307,10 +305,10 @@ class LTSeriesProtocol extends FMICProtocolBase {
       byte[] packetBuffer = new byte[64];
       int packetBytesRead = m_device.read(packetBuffer,500);
       if (packetBytesRead < 0) {
-        log(BaseExample.ANSI_RED,"read failed, error=" + m_device.getLastErrorMessage());
+        log("read failed, error=" + m_device.getLastErrorMessage());
         return STATUS_READ_FAIL;
       } else if(packetBytesRead!=64) {
-        log(BaseExample.ANSI_RED,"read incomplete, error=" + m_device.getLastErrorMessage());
+        log("read incomplete, error=" + m_device.getLastErrorMessage());
         return STATUS_READ_FAIL;
       } else {
         FMICStartupExample.printAsHex2(packetBuffer,">");
@@ -362,25 +360,24 @@ class LTSeriesProtocol extends FMICProtocolBase {
       if (psJsonStatus!=STATUS_OK) {
         return psJsonStatus;
       }
-      log(BaseExample.ANSI_BLUE,presetJsonSB.toString());
+      log(presetJsonSB.toString());
     }
-    // We don't expect to get this far
     return STATUS_OK;
   }
 
   private int sendCommand(String commandBytesHex, String commandDescription) {
     byte[] commandBytes = new byte[64];
     colonSeparatedHexToByteArray(commandBytesHex, commandBytes);
-    log(BaseExample.ANSI_GREEN, "Sending " + commandDescription);
+    log( "Sending " + commandDescription);
     FMICStartupExample.printAsHex2(commandBytes,"<");
     int bytesWritten = m_device.write(commandBytes, 64, (byte) 0x00, true);
     if (bytesWritten < 0) {
-      log(BaseExample.ANSI_RED,m_device.getLastErrorMessage());
+      log(m_device.getLastErrorMessage());
       return STATUS_WRITE_FAIL;
     }
     int bytesRead = readAndAssembleResponsePackets();
     if (bytesRead < 0) {
-      log(BaseExample.ANSI_RED,m_device.getLastErrorMessage());
+      log(m_device.getLastErrorMessage());
       return STATUS_REASSEMBLY_FAIL;
     } 
     return STATUS_OK;
@@ -423,7 +420,7 @@ class LTSeriesProtocol extends FMICProtocolBase {
         assert payloadLength == firmwareVersionLength + 2;
 
         String firmwareVersion=new String(assembledResponseMessage,7,firmwareVersionLength);
-        log(BaseExample.ANSI_BLUE,"Firmware version: " + firmwareVersion);
+        log("Firmware version: " + firmwareVersion);
     } else if(
       (0xfa == (0xff & assembledResponseMessage[2]) ) &&
       (0x01 == (0xff & assembledResponseMessage[3]) )
